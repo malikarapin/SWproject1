25dc84a8d23972a3c160bc2364b9939aff7e8b39
malikarapin <badboy.16062535@gmail.com>
2015-01-23 13:18:47 +0700

    login to mysql Base

--- Student/src/acsm/student/StudentLogin.java
+++ Student/src/acsm/student/StudentLogin.java
@@ -1,41 +1,31 @@
 package acsm.student;
 
 
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+
 import java.util.ArrayList;
 import java.util.List;
-import android.app.AlertDialog;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONException;
-import org.json.JSONObject;
-import android.os.StrictMode;
-import android.annotation.SuppressLint;
-import android.util.Log;
-import android.view.Menu;
 
 
 
@@ -47,112 +37,118 @@ public class StudentLogin extends Activity {
 	String sUsername, sPassword;
 	public static int agID;
 	Toast toast;
+	ProgressDialog dialog = null;
+	 HttpResponse response;
+	    HttpClient httpclient;
+	    HttpPost httppost;
+	    StringBuffer buffer;
+	    List<NameValuePair> nameValuePairs;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.student_login);
 		
-		final AlertDialog.Builder ad = new AlertDialog.Builder(this);
+		//final AlertDialog.Builder ad = new AlertDialog.Builder(this);
 
-		final EditText etUsername = (EditText) findViewById(R.id.userlog);
-		final EditText etPassword = (EditText) findViewById(R.id.passlog);
-
-		final Button btnlogin = (Button) findViewById(R.id.submit);
-		btnlogin.setOnClickListener(new OnClickListener() {
-			public void onClick(View v){
-				String url = "192.168.208.237/PHPContodatabase/checkLogin.php";
-        		List<NameValuePair> params = new ArrayList<NameValuePair>();
-                params.add(new BasicNameValuePair("strUser", etUsername.getText().toString()));
-                params.add(new BasicNameValuePair("strPass", etPassword.getText().toString()));
-                
-                /** Get result from Server (Return the JSON Code)
-                 * StatusID = ? [0=Failed,1=Complete]
-                 * MemberID = ? [Eg : 1]
-                 * Error	= ?	[On case error return custom error message]
-                 * 
-                 * Eg Login Failed = {"StatusID":"0","MemberID":"0","Error":"Incorrect Username and Password"}
-                 * Eg Login Complete = {"StatusID":"1","MemberID":"2","Error":""}
-                 */
-                
-            	String resultServer  = getHttpPost(url,params);
-                
-                /*** Default Value ***/
-            	String strStatusID = "0";
-            	String strMemberID = "0";
-            	String strError = "Unknow Status!";
-            	
-            	JSONObject c;
-				try {
-					c = new JSONObject(resultServer);
-	            	strStatusID = c.getString("StatusID");
-	            	strMemberID = c.getString("MemberID");
-	            	strError = c.getString("Error");
-	            	
-				} catch (JSONException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+		//final EditText etUsername = (EditText) findViewById(R.id.userlog);
+		//final EditText etPassword = (EditText) findViewById(R.id.passlog);
+		final Button submit = (Button) findViewById(R.id.submit);
+		
+		submit.setOnClickListener(new OnClickListener() {
+			
+			 @Override
+	            public void onClick(View v) {
+	               // dialog = ProgressDialog.show(StudentLogin.this, "", 
+	                 //       "Validating user...", true);
 				
-				
-				
-				// Prepare Login
-				if(strStatusID.equals("0"))
-				{
-					// Dialog
-					ad.setTitle("Error! ");
-					ad.setIcon(android.R.drawable.btn_star_big_on); 
-					ad.setPositiveButton("Close", null);
-					ad.setMessage(strError);
-					ad.show();
-					etUsername.setText("");
-					etPassword.setText("");
-				}
-				else
-				{
-					Toast.makeText(StudentLogin.this, "Login OK", Toast.LENGTH_SHORT).show();
-					Intent newActivity = new Intent(StudentLogin.this,StudentMenu.class);
-					newActivity.putExtra("MemberID", strMemberID);
-					startActivity(newActivity);
-				}
-           	            
-            }
+	                 new Thread(new Runnable() {
+	                        public void run() {
+	                            login();                          
+	                        }
+	                      }).start();               
+	            }
         });
         
 	}
-	public String getHttpPost(String url,List<NameValuePair> params) {
-		StringBuilder str = new StringBuilder();
-		HttpClient client = new DefaultHttpClient();
-		HttpPost httpPost = new HttpPost(url);
-		
-		try {
-			httpPost.setEntity(new UrlEncodedFormEntity(params));
-			HttpResponse response = client.execute(httpPost);
-			StatusLine statusLine = response.getStatusLine();
-			int statusCode = statusLine.getStatusCode();
-			if (statusCode == 200) { // Status OK
-				HttpEntity entity = response.getEntity();
-				InputStream content = entity.getContent();
-				BufferedReader reader = new BufferedReader(new InputStreamReader(content));
-				String line;
-				while ((line = reader.readLine()) != null) {
-					str.append(line);
-				}
-			} else {
-				Log.e("Log", "Failed to download result..");
-			}
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return str.toString();
-	}
+	
+	void login(){
+        try{            
+        	final EditText etUsername = (EditText) findViewById(R.id.userlog);
+    		final EditText etPassword = (EditText) findViewById(R.id.passlog);
+            httpclient=new DefaultHttpClient();
+            httppost= new HttpPost("http://192.168.208.253/PHPContodatabase/checkLogin.php"); // make sure the url is correct.
+            //Log.d("Log d", "Test Log");
+			// Log.i("Log d", "Test Log");
+			// Log.e("Log d", "Test Log");
+            //add your data
+            nameValuePairs = new ArrayList<NameValuePair>(2);
+            // Always use the same variable name for posting i.e the android side variable name and php side variable name should be similar, 
+            nameValuePairs.add(new BasicNameValuePair("std_id",etUsername.getText().toString().trim()));  // $Edittext_value = $_POST['Edittext_value'];
+            nameValuePairs.add(new BasicNameValuePair("std_pwd",etPassword.getText().toString().trim())); 
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            //Execute HTTP Post Request
+            response=httpclient.execute(httppost);
+            // edited by James from coderzheaven.. from here....
+            ResponseHandler<String> responseHandler = new BasicResponseHandler();
 
-	public void beginLogin() {
-		// TODO Auto-generated method stub
-		Intent tomenu = new Intent(getApplicationContext(), StudentMenu.class);
+            final String response = httpclient.execute(httppost, responseHandler);
+         System.out.println("Response : " + response); 
+   
+            runOnUiThread(new Runnable() {
+            	 public void run() {
+            		 
+            		 if(response.equalsIgnoreCase("User Found")){
+            			// Intent i = new Intent(getApplicationContext(),StudentMenu.class);
+                 		//startActivity(i);
+                 		Toast.makeText(StudentLogin.this,"NO Login Success", Toast.LENGTH_SHORT).show();
+            			 
+            		 }else{
+                        	//Toast.makeText(StudentLogin.this,"NO Login Success", Toast.LENGTH_SHORT).show();
+                        	Intent i = new Intent(getApplicationContext(),StudentMenu.class);
+                     		startActivity(i);
+                        }
+            		 
+            		 
+            	 }
+            });
 
-		startActivity(tomenu);
-	}
+             
+        }catch(Exception e){
+            dialog.dismiss();
+            System.out.println("Exception : " + e.getMessage());
+        }
+    }
+	
+	  
+/*
+	 @Override
+	    public void onBackPressed() {
+	 
+	        AlertDialog.Builder alertDialog = new AlertDialog.Builder(StudentLogin.this);
+	 
+	        alertDialog.setTitle("Confirm Exit...");
+	        alertDialog.setMessage("คุณต้องการออกจากโปรแกรมหรือไม่ ?");
+	        alertDialog.setIcon(R.drawable.ic_launcher);
+	 
+	        alertDialog.setPositiveButton("ใช่",
+	                new DialogInterface.OnClickListener() {
+	                    public void onClick(DialogInterface dialog,int which) {
+	                        //คลิกใช่ ออกจากโปรแกรม
+	                        finish();
+	                        StudentLogin.super.finishAffinity();
+	                    }
+	                });
+	 
+	        alertDialog.setNegativeButton("ไม่",
+	                new DialogInterface.OnClickListener() {
+	                    public void onClick(DialogInterface dialog,    int which) {
+	                        //คลิกไม่ cancel dialog
+	                        dialog.cancel();
+	                    }
+	                });
+	 
+	        alertDialog.show();
+	}*/
+	
 }
